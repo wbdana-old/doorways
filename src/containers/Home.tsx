5ca76b8d30613969caffaf7dd312d30e909011dd
William Dana <william.b.dana@gmail.com>
2019-01-29 13:29:27 -0500

    Add type definitions; add redux; build out src folder structure

---
+++ src/containers/Home.tsx
@@ -0,0 +1,5 @@
+import Home from './components/Home';
+import * as actions from '../actions/';
+import { StoreState } from '../types/index';
+import { connect } from 'react-redux';
+import { Dispatch } from 'redux';
