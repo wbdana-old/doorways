a260c27cd844fb83fc18e1249b2955a0b5282462
William Dana <william.b.dana@gmail.com>
2019-01-30 00:34:35 -0500

    Update Home to class component; add keypress handling

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,26 +5,41 @@ import { HomeContainer, PromptInput } from '../styled/components/home';
 export interface IProps {
     command: string,
     updateCommand: (payload: string) => void;
+    submitCommand: () => void;
 }
 
-function Home({ command, updateCommand }: IProps) {
-    const handleChange = (e: any) => {
-        updateCommand(e.target.value);
+class Home extends React.Component<IProps> {
+    handleChange = (e: any) => {
+        this.props.updateCommand(e.target.value);
     };
 
-    return (
-        <HomeContainer>
-            <Rotate>
-                $ {command}
-            </Rotate>
-            <div>
-                $ {command}
-            </div>
-            <div className={"prompt"}>
-                $ <PromptInput type={"text"} onChange={handleChange} value={command} />
-            </div>
-        </HomeContainer>
-    );
+    handleKeyPress = (e: any) => {
+        if (e.keyCode === 13) {
+            console.log('hit enter');
+        }
+    };
+
+    render() {
+        const { command } = this.props;
+        return (
+            <HomeContainer>
+                <Rotate>
+                    $ {command}
+                </Rotate>
+                <div>
+                    $ {command}
+                </div>
+                <div className={"prompt"}>
+                    $ <PromptInput
+                    type={"text"}
+                    value={command}
+                    onChange={this.handleChange}
+                    onKeyDown={this.handleKeyPress}
+                />
+                </div>
+            </HomeContainer>
+        );
+    }
 }
 
 export default Home;
