5e5bf6d6a4a128bc02bd1a58ebc9f4ac9b0b9c34
William Dana <william.b.dana@gmail.com>
2019-01-31 14:59:02 -0500

    Lint App

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,15 @@ import Terminal from './containers/Terminal';
 import './App.css';
 
 class App extends React.Component {
-  public render() {
-    return (
-        <div className={"App"}>
-          <Terminal />
-        </div>
-    );
-  }
+
+
+    public render() {
+        return (
+            <div className={"App"}>
+                <Terminal />
+            </div>
+        );
+    }
 }
 
 export default App;
