98dcc62b57c150f94a93017f5d3b0818cae32a0c
William Dana <william.b.dana@gmail.com>
2019-02-07 14:07:47 -0500

    Open MENU_OPTIONS links in new tabs

--- src/components/Gui/MenuOptions.tsx
+++ src/components/Gui/MenuOptions.tsx
@@ -3,32 +3,32 @@ export const MENU_OPTIONS = [
         title: 'Projects',
         subs: [
             {
-                callback: () => window.open('https://asteroids-online.herokuapp.com/', '_blank', 'noopener noreferrer'),
+                callback: () => window.open('https://asteroids-online.herokuapp.com/', '_blank'),
                 title: 'Asteroids Online',
                 url: 'https://asteroids-online.herokuapp.com/',
             },
             {
-                callback: () => window.open('https://conntext.herokuapp.com/', '_blank', 'noopener noreferrer'),
+                callback: () => window.open('https://conntext.herokuapp.com/', '_blank'),
                 title: 'Connected Text',
                 url: 'https://conntext.herokuapp.com/',
             },
             {
-                callback: () => window.open('https://pypi.org/project/opengraphio/', '_blank', 'noopener noreferrer'),
+                callback: () => window.open('https://pypi.org/project/opengraphio/', '_blank'),
                 title: 'OpenGraph.io Python Client',
                 url: 'https://pypi.org/project/opengraphio/',
             },
             {
-                callback: () => window.open('https://rubygems.org/gems/opengraph-io/', '_blank', 'noopener noreferrer'),
+                callback: () => window.open('https://rubygems.org/gems/opengraph-io/', '_blank'),
                 title: 'OpenGraph.io Ruby Client',
                 url: 'https://rubygems.org/gems/opengraph-io/',
             },
             {
-                callback: () => window.open('https://qsmart-react.herokuapp.com/', '_blank', 'noopener noreferrer'),
+                callback: () => window.open('https://qsmart-react.herokuapp.com/', '_blank'),
                 title: 'QSmart',
                 url: 'https://qsmart-react.herokuapp.com/',
             },
             {
-                callback: () => window.open('https://pixel-playground.herokuapp.com/', '_blank', 'noopener noreferrer'),
+                callback: () => window.open('https://pixel-playground.herokuapp.com/', '_blank'),
                 title: 'Pixel Playground',
                 url: 'https://pixel-playground.herokuapp.com/',
             },
@@ -47,15 +47,15 @@ export const MENU_OPTIONS = [
         subs: [
             {
                 title: 'GitHub',
-                callback: () => window.open('https://github.com/wbdana/', '_blank', 'noopener noreferrer'),
+                callback: () => window.open('https://github.com/wbdana/', '_blank'),
             },
             {
                 title: 'LinkedIn',
-                callback: () => window.open('https://linkedin.com/in/wbdana/', '_blank', 'noopener noreferrer'),
+                callback: () => window.open('https://linkedin.com/in/wbdana/', '_blank'),
             },
             {
                 title: 'Twitter',
-                callback: () => window.open('https://twitter.com/_wbdana', '_blank', 'noopener noreferrer'),
+                callback: () => window.open('https://twitter.com/_wbdana', '_blank'),
             },
         ]
     }
